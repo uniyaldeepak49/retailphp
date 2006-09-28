4ae1621c70f566890b7ad3b780bb9c9eca1f1ade
bmbommarito <bmbommarito@62206adb-801e-0410-8f51-4d43eb745e24>
2006-09-28 11:34:40 +0000

    Set the version of RetailPHP to '1.0d1'.
    Added two files, one is the standard 'VERSION' file showing the current version of the tarball and the other is 'includes/version.inc.php' which is a global constant that is used throughout the script to show version.

---
+++ trunk/includes/version.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+// Defines the current version number of the script so that I do not have to
+   change many PHP files when I update the version number.
+
+constant("CURRENTVERSION",    "1.0d1")
+
+?>
